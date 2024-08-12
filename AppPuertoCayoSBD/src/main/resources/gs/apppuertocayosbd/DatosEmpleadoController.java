6fe7621e31d9c9f00f3145d1b82898050caba130
Gabriel Salas <149934515+gasaesco@users.noreply.github.com>
2024-08-11 20:07:32 -0500

    botones cargados

---
+++ AppPuertoCayoSBD/src/main/resources/gs/apppuertocayosbd/DatosEmpleadoController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package gs.apppuertocayosbd;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author gasaesco
+ */
+public class DatosEmpleadoController implements Initializable {
+
+    @FXML
+    private TextField campoCed;
+    
+    @FXML
+    private TextField campoNombres;
+    
+    @FXML
+    private TextField campoApellidos;
+    
+    @FXML
+    private TextField campoTelf;
+    
+    @FXML
+    private TextField campoCorreo;
+    
+    @FXML
+    private TextField campoSueldo;
+    
+    @FXML
+    private TextField campoJefe;
+    
+    @FXML
+    private TableView tablaDatos;
+    
+    @FXML
+    private Button btnGuardar;
+    
+    @FXML
+    private Button btnModificar;
+    
+    @FXML
+    private Button btnEliminar;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
